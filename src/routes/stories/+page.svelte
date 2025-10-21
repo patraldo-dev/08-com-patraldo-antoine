e7a435e1cc37deab7ee7521ec32a5022958a07aa
Chef Tech <cheftech@patraldo.com>
2025-10-21 16:05:20 -0600

    what else did I fix or did I not fix

--- src/routes/stories/+page.svelte
+++ src/routes/stories/+page.svelte
@@ -1,12 +1,11 @@
 <script>
   import { t } from '$lib/translations';
-  // No need for const $t = t if it works without it
 </script>
 
 <svelte:head>
   <title>{$t('pages.stories.title')}</title>
   <meta name="description" content={$t('pages.stories.description')} />
-</script>
+</svelte:head>
 
 <div class="layout">
   <h1>{$t('pages.stories.title')}</h1>
