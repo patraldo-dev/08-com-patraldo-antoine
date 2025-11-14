ac7014c49506e23dd7b4867a6ae4a4840beaaad0
Chef Tech <cheftech@patraldo.com>
2025-11-13 19:51:35 -0600

    remove placeholder picsum from page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,11 +18,6 @@
     selectedArtwork = null;
   }
 
-  // Add placeholder images for artworks
-  $: displayArtworks = data?.artworks?.map(artwork => ({
-    ...artwork,
-    thumbnailUrl: artwork.thumbnailUrl || `https://picsum.photos/400/300?random=${artwork.id}`
-  })) || [];
 
 // Title handling
   $: siteTitle = $t('site.title');
