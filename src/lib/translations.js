d69af3c7adf8cc8b46dec02a33868f3b53268f9a
Chef Tech <cheftech@patraldo.com>
2025-11-18 17:43:40 -0600

    add wildcards to page routes

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -10,41 +10,33 @@ const config = {
   fallbackLocale: 'es-MX',
 
   loaders: [
-    // COMMON STRINGS (shared across all pages)
-    { locale: 'es-MX', key: 'common', loader: () => import('./locales/es-MX/common.json') },
-    { locale: 'en-US', key: 'common', loader: () => import('./locales/en-US/common.json') },
-    { locale: 'fr-CA', key: 'common', loader: () => import('./locales/fr-CA/common.json') },
+
+// COMMON STRINGS (shared across all pages; but in each page, pages.home.meta.title for browser tab)
+{ locale: 'es-MX', key: 'common', loader: () => import('./locales/es-MX/common.json') },
+{ locale: 'en-US', key: 'common', loader: () => import('./locales/en-US/common.json') },
+{ locale: 'fr-CA', key: 'common', loader: () => import('./locales/fr-CA/common.json') },
 
 // MAIN SITE PAGES
 { locale: 'es-MX', key: 'pages.home', loader: () => import('./locales/es-MX/pages/home.json') },
 { locale: 'en-US', key: 'pages.home', loader: () => import('./locales/en-US/pages/home.json') },
 { locale: 'fr-CA', key: 'pages.home', loader: () => import('./locales/fr-CA/pages/home.json') },
 
+// STORYVIEW PAGES - includes WILDCARD and pages.home.meta.title for browser tab 
 
-    { locale: 'es-MX', key: 'pages.about', routes: ['/about'], loader: () => import('./locales/es-MX/pages/about.json') },
-    { locale: 'en-US', key: 'pages.about', routes: ['/about'], loader: () => import('./locales/en-US/pages/about.json') },
-    { locale: 'fr-CA', key: 'pages.about', routes: ['/about'], loader: () => import('./locales/fr-CA/pages/about.json') },
-
-//    { locale: 'es-MX', key: 'pages.contact', routes: ['/contact'], loader: () => import('./locales/es-MX/pages/contact.json') },
-//    { locale: 'en-US', key: 'pages.contact', routes: ['/contact'], loader: () => import('./locales/en-US/pages/contact.json') },
-//    { locale: 'fr-CA', key: 'pages.contact', routes: ['/contact'], loader: () => import('./locales/fr-CA/pages/contact.json') },
-
-
-    // STORYVIEW PAGES
 { locale: 'es-MX', key: 'pages.stories', routes: ['/stories', '/stories/*'], loader: () => import('./locales/es-MX/pages/stories.json') },
 { locale: 'en-US', key: 'pages.stories', routes: ['/stories', '/stories/*'], loader: () => import('./locales/en-US/pages/stories.json') },
 { locale: 'fr-CA', key: 'pages.stories', routes: ['/stories', '/stories/*'], loader: () => import('./locales/fr-CA/pages/stories.json') },
 
-// COLLECTION PAGE
-{ locale: 'es-MX', key: 'pages.collection', routes: ['/collection'], loader: () => import('./locales/es-MX/pages/collection.json') },
-{ locale: 'en-US', key: 'pages.collection', routes: ['/collection'], loader: () => import('./locales/en-US/pages/collection.json') },
-{ locale: 'fr-CA', key: 'pages.collection', routes: ['/collection'], loader: () => import('./locales/fr-CA/pages/collection.json') },
+// COLLECTION PAGE - includes WILDCARD and pages.home.meta.title for browser tab 
 
-// TOOLS PAGE
-{ locale: 'es-MX', key: 'pages.tools', routes: ['/tools'], loader: () => import('./locales/es-MX/pages/tools.json') },
-{ locale: 'en-US', key: 'pages.tools', routes: ['/tools'], loader: () => import('./locales/en-US/pages/tools.json') },
-{ locale: 'fr-CA', key: 'pages.tools', routes: ['/tools'], loader: () => import('./locales/fr-CA/pages/tools.json') },
+{ locale: 'es-MX', key: 'pages.collection', routes: ['/collection', '/collection/*'], loader: () => import('./locales/es-MX/pages/collection.json') },
+{ locale: 'en-US', key: 'pages.collection', routes: ['/collection', '/collection/*'], loader: () => import('./locales/en-US/pages/collection.json') },
+{ locale: 'fr-CA', key: 'pages.collection', routes: ['/collection', '/collection/*'], loader: () => import('./locales/fr-CA/pages/collection.json') },
 
+// TOOLS PAGE - includes WILDCARD and pages.home.meta.title for browser tab 
+{ locale: 'es-MX', key: 'pages.tools', routes: ['/tools', '/tools/*'], loader: () => import('./locales/es-MX/pages/tools.json') },
+{ locale: 'en-US', key: 'pages.tools', routes: ['/tools', '/tools/*'], loader: () => import('./locales/en-US/pages/tools.json') },
+{ locale: 'fr-CA', key: 'pages.tools', routes: ['/tools', '/tools/*'], loader: () => import('./locales/fr-CA/pages/tools.json') },
 
   ],
 };
