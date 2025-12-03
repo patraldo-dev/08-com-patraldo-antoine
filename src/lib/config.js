d2db176a9a453f66aed04f1a809acd5dcfa19bdd
Chef Tech <cheftech@patraldo.com>
2025-12-03 15:56:16 -0600

    add canal route

--- src/lib/config.js
+++
@@ -1,3 +0,0 @@
-// Cloudflare Images configuration
-export const CF_IMAGES_ACCOUNT_HASH = "4bRSwPonOXfEIBVZiDXg0w"; // Replace with your actual hash
-export const CUSTOM_DOMAIN = "antoine.patraldo.com"; // Your custom domain
