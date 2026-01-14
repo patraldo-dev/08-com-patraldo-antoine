741e6a02c2a82a2a4896e69ade4240ea263a75a3
Chef Tech <cheftech@patraldo.com>
2026-01-13 22:35:43 -0600

    back to Perplexity's lighting

--- src/lib/components/Image3DManipulator.svelte
+++ src/lib/components/Image3DManipulator.svelte
@@ -44,13 +44,8 @@
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
     
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
-    
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    directionalLight.position.set(1, 1, 1);
-    scene.add(directionalLight);
-    
+    scene.add(new THREE.AmbientLight(0xffffff, 1.2));
+
     const gridHelper = new THREE.GridHelper(10, 10, 0xcccccc, 0xe0e0e0);
     scene.add(gridHelper);
   }
