8cb729017086f7beecff77a57fd792bbf0c10327
Chef Tech <cheftech@patraldo.com>
2026-05-04 13:23:46 -0600

    remove broken NLWebSearch from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
   import { page } from '$app/stores';
   import { locale, setLocale } from '$lib/i18n';
   import Navigation from '$lib/components/Navigation.svelte';
-  import NLWebSearch from '$lib/components/NLWebSearch.svelte';
   import AIArtAssistant from '$lib/components/AIArtAssistant.svelte';
   import { browser } from '$app/environment';
   
@@ -94,7 +93,6 @@
   <footer>
     <p>&copy; 2026 Antoine Patraldo. All rights reserved.</p>
   </footer>
-<NLWebSearch />
 <AIArtAssistant />
 </div>
 
